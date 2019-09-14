988ec64df1d5fa1465fbcd2d73e85f817c417fd0
Dhara <48283538+dharababariya@users.noreply.github.com>
2019-09-14 18:02:39 +0530

    Update get_otp.js

--- routes/get_otp.js
+++ routes/get_otp.js
@@ -13,98 +13,103 @@ const otp_generate = async (req, res, next) => {
 
     try {
 
-        const result = await knex('public.send_otp')
-            .where('username', req.body.username); 
-            
-            const data = req.body;
+        const input = {
+            username: req.body.username,
+        };
 
-            Joi.validate(data, schema, async (err, value) => {
+        const data = req.body;
 
-                if (err) {
-                    // send a 422 error response if validation fails
-                    res.status(422).json({
-        
-        
-                        meta: {
-                            status:'0',
-                            message: `Enter Valid Username ${err.message}`
-                        },
-                        data: {
-            
-                        }
-                        
-                    });
-                 
+        Joi.validate(data, schema, async (err, value) => {
 
-                 } 
-                })
-                 
-                  if (result.length === 0) {
-                        
+            if (err) {
+                // send a 422 error response if validation fails
+                res.status(422).json({
 
-                            await register_user(req.body.username);
 
-                            const otp = await generateOTP();
+                    meta: {
+                        status: '0',
+                        message: `Enter Valid Username ${err.message}`
+                    },
+                    data: {
 
-                            const add_otp_in_db = await knex('public.send_otp')
-                                .update("otp", otp)
-                                .update('updated_at', moment())
-                                .where("username", "=", req.body.username)
-                                .returning("*")
-
-                            await send_sms(otp);
-
-                            await send_mail(otp);
-
-                            res
-                                .status(201)
-                                .json({   
-                                    meta: 
-                                    {
-                                            status:'1',
-                                            message: 'Successfully Crated User And Send Otp'
-                                },
-                                data: {
-                    
-                                } })
-
-                    } else {
-
-                        const otp = await generateOTP();
-
-                        const result = await knex("public.send_otp")
-                            .update("otp", otp)
-                            .update('updated_at', moment())
-                            .where("username", "=", req.body.username)
-                            .returning("*")
-                        
-                        
-                        await send_sms(otp);
-
-                        await send_mail(otp);
-
-                        res
-                            .status(200)
-                            .json({ 
-                                meta: 
-                                    {
-                                            status:'2',
-                                            message: 'Successfully Send Otp'
-                                    },
-                                data: {
-                    
-                                }
-                             })
                     }
-               
+
+                });
+
+
+            }
+        })
+
+        const result = await knex('public.send_otp')
+        .where('username', input.username);
+
+
+        if (result.length === 0) {
+
+            await register_user(input.username);
+
+            const otp = await generateOTP();
+
+            const add_otp_in_db = await knex('public.send_otp')
+                .update("otp", otp)
+                .update('updated_at', moment())
+                .where("username", "=", req.body.username)
+                .returning("*")
+
+            await send_sms(input, otp);
+
+            await send_mail(otp);
+
+            res
+                .status(201)
+                .json({
+                    meta:
+                    {
+                        status: '1',
+                        message: 'Successfully Crated User And Send Otp'
+                    },
+                    data: {
+
+                    }
+                })
+
+        } else {
+
+            const otp = await generateOTP();
+
+            const result = await knex("public.send_otp")
+                .update("otp", otp)
+                .update('updated_at', moment())
+                .where("username", "=", req.body.username)
+                .returning("*")
+
+
+            await send_sms(input, otp);
+
+            await send_mail(otp);
+
+            res
+                .status(200)
+                .json({
+                    meta:
+                    {
+                        status: '2',
+                        message: 'Successfully Send Otp'
+                    },
+                    data: {
+
+                    }
+                })
+        }
+
 
     } catch (error) {
         // console.error(error)
-        
+
         return res.status(424).json({
             meta: {
-                    status:'3',
-                    message: `Failed ${error.message}`
+                status: '3',
+                message: `Failed ${error.message}`
             },
             data: {
 
@@ -132,7 +137,7 @@ const register_user = async (username) => {
     const data = {
         username: username,
         otp: '',
-        created_at : moment(),
+        created_at: moment(),
         updated_at: moment(),
     }
 
@@ -143,37 +148,40 @@ const register_user = async (username) => {
 }
 
 // create new contact
-const send_sms = async (otp) => {
+const send_sms = async (data, otp) => {
 
     try {
         const accountSid = 'AC634d0da3713aa9e0ec78be79b32c8daa';
         const authToken = '51a9c2d9150f044203a2260ae14440d3';
         const client = require('twilio')(accountSid, authToken);
-        let resMsg = '';
+        let resMsg = ''
 
-       //  console.log(otp);
+        const mobile_number = `+91${data.username}`
+
         await client
             .messages
-            .create({ body: otp, from: '+16029754928', to: '+919574335333' })
+            .create({ body: otp, from: '+16029754928', to: mobile_number})
             .then(message => {
                 resMsg = message.sid;
-                console.log(JSON.stringify(message, 5));
+                console.log(`-----------------------------------------`);
+                console.log('Message sent successfully to user');
+                console.log(`-----------------------------`)
             })
             .done();
         // res.json(resMsg);
 
     } catch (error) {
-       // throw error;
+        // throw error;
 
-       return res.status(424).json({
-        meta: {
-            status:'3',
-            message: `Failed ${error.message}`
-        },
-        data: {
+        return res.status(424).json({
+            meta: {
+                status: '3',
+                message: `Failed ${error.message}`
+            },
+            data: {
 
-        }
-    })
+            }
+        })
 
     }
 }
@@ -183,35 +191,40 @@ const send_mail = async (otp) => {
     try {
 
         //    Prepare payload for send email
-    const msg = {
-        to: 'dharababariya34@gmail.com',
-        from: 'nmd882@gmail.com',
-        subject: 'Sending with Twilio SendGrid is Fun',
-        text: 'send otp',
-        html: `<strong>Thank You for registered with us. This is your otp ${otp}</strong>`
-    };
+        const msg = {
+            to: 'dharababariya34@gmail.com',
+            from: 'nmd882@gmail.com',
+            subject: 'Sending with Twilio SendGrid is Fun',
+            text: 'send otp',
+            html: `<strong>Thank You for registered with us. This is your otp ${otp}</strong>`
+        };
 
-    //    Send email
-    sgMail.send(msg);
-        
+        //    Send email
+        sgMail.send(msg);
+
     } catch (error) {
 
         return res.status(424).json({
             meta: {
-                status:'3',
+                status: '3',
                 message: `Failed ${error.message}`
             },
             data: {
-    
+
             }
         })
-        
+
     }
-    
+
 }
+
+
 const schema = Joi.object({
     username: Joi.string().alphanum().min(3).max(16).required(),
 })
+
+
+
 // otp generate api
 router.put('/api/otp/generate_otp', otp_generate);
 
