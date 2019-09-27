56f6a21095be84d93929499d10431a3f314da0fb
Dhara <dharababariya34@gmail.com>
2019-09-27 17:33:35 +0530

    change knex config

--- helper/knex.js
+++ helper/knex.js
@@ -2,11 +2,17 @@ const  knex = require('knex')({
     client: 'pg',
     version: '7.2',
     connection: {
-      host : '127.0.0.1',
+      host : 'localhost',
       user : 'postgres',
       password : 'postgres',
       database : 'user_registration'
-    }
+    },
+    pool: {
+      min: 1,
+      max: 2,
+    },
+    ssl: true,
+    debug: false
   });
   //expose knex connection object;
   module.exports = knex;
